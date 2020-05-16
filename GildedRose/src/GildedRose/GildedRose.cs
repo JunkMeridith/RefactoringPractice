e5e103145aead3f0bb8625e315449166483227cd
MeridithJunk <Meridith.a.junk@accenture.com>
2020-05-16 16:08:58 -0500

    Shotgun surgery - inlining method to help remove conditional complexity for Aged Brie

--- GildedRose/src/GildedRose/GildedRose.cs
+++ GildedRose/src/GildedRose/GildedRose.cs
@@ -22,14 +22,78 @@
         {
             if (item.Name.Equals("Aged Brie"))
             {
-                ShotGun(item);
+                if (!item.Name.Equals("Aged Brie")
+                    && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                        {
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+
+                        if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                        {
+                            if (item.SellIn < 11)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality = item.Quality + 1;
+                                }
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality = item.Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                {
+                    item.SellIn = item.SellIn - 1;
+                }
+
+                if (item.SellIn < 0)
+                {
+                    if (!item.Name.Equals("Aged Brie"))
+                    {
+                        if (!item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                        {
+                            if (item.Quality > 0)
+                            {
+                                if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                                {
+                                    item.Quality = item.Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            item.Quality = item.Quality - item.Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+                        }
+                    }
+                }
             }
 
-            ShotGun(item);
-        }
-
-        private static void ShotGun(Item item)
-        {
             if (!item.Name.Equals("Aged Brie")
                 && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
             {
