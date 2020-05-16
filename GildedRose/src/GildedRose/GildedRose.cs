f111c1efa3c251631685660bb9fd76c580fa3a94
MeridithJunk <Meridith.a.junk@accenture.com>
2020-05-16 16:56:03 -0500

    Switch statement -> replace conditional with polymophism part 2: moving Update item to the items class

--- GildedRose/src/GildedRose/GildedRose.cs
+++ GildedRose/src/GildedRose/GildedRose.cs
@@ -14,72 +14,7 @@
             for (int i = 0; i < Items.Length; i++)
             {
                 var item = Items[i];
-                UpdateItem(item);
-            }
-        }
-
-        private static void UpdateItem(Item item)
-        {
-            switch (item.Name)
-            {
-                case "Aged Brie":
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item.SellIn < 0 && item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-
-                        if (item.SellIn < 11 && item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-
-                        if (item.SellIn < 6 && item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-
-                    break;
-                case "Sulfuras, Hand of Ragnaros": break;
-                default:
-                    if (item.Quality > 0)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            item.Quality = item.Quality - 1;
-                        }
-                    }
-                    break;
+                item.UpdateItem();
             }
         }
     }
--- GildedRose/src/GildedRose/Item.cs
+++ GildedRose/src/GildedRose/Item.cs
@@ -22,5 +22,71 @@ namespace GildedRose
         {
             return $"{Name}, {SellIn}, {Quality}";
         }
+
+        public void UpdateItem()
+        {
+            switch (this.Name)
+            {
+                case "Aged Brie":
+                    if (this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+                    }
+
+                    this.SellIn = this.SellIn - 1;
+
+                    if (this.SellIn < 0 && this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+                    }
+
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                {
+                    if (this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+
+
+                        if (this.SellIn < 11 && this.Quality < 50)
+                        {
+                            this.Quality = this.Quality + 1;
+                        }
+
+                        if (this.SellIn < 6 && this.Quality < 50)
+                        {
+                            this.Quality = this.Quality + 1;
+                        }
+                    }
+                }
+
+                    this.SellIn = this.SellIn - 1;
+
+                    if (this.SellIn < 0)
+                    {
+                        this.Quality = this.Quality - this.Quality;
+                    }
+
+                    break;
+                case "Sulfuras, Hand of Ragnaros": break;
+                default:
+                    if (this.Quality > 0)
+                    {
+                        this.Quality = this.Quality - 1;
+                    }
+
+                    this.SellIn = this.SellIn - 1;
+
+                    if (this.SellIn < 0)
+                    {
+                        if (this.Quality > 0)
+                        {
+                            this.Quality = this.Quality - 1;
+                        }
+                    }
+                    break;
+            }
+        }
     }
+    
 }
