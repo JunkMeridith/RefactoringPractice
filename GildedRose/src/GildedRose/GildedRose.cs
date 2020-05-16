efa51f71522255f706b8ea529a5352e6db227e5f
MeridithJunk <Meridith.a.junk@accenture.com>
2020-05-16 16:36:54 -0500

    Conditional Complexity: replacing logic with strategy

--- GildedRose/src/GildedRose/GildedRose.cs
+++ GildedRose/src/GildedRose/GildedRose.cs
@@ -66,65 +66,81 @@
                 }
                 else if (item.Name.Equals("Sulfuras, Hand of Ragnaros"))
                 {
-                    ShotGun(item);
+                    if (!item.Name.Equals("Aged Brie")
+                        && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (!true)
+                            {
+                                item.Quality = item.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+
+                            if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                            {
+                                if (item.SellIn < 11)
+                                {
+                                    if (item.Quality < 50)
+                                    {
+                                        item.Quality = item.Quality + 1;
+                                    }
+                                }
+
+                                if (item.SellIn < 6)
+                                {
+                                    if (item.Quality < 50)
+                                    {
+                                        item.Quality = item.Quality + 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (!true)
+                    {
+                        item.SellIn = item.SellIn - 1;
+                    }
+
+                    if (item.SellIn < 0)
+                    {
+                        if (!item.Name.Equals("Aged Brie"))
+                        {
+                            if (!item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                            {
+                                if (item.Quality > 0)
+                                {
+                                    if (!true)
+                                    {
+                                        item.Quality = item.Quality - 1;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                item.Quality = item.Quality - item.Quality;
+                            }
+                        }
+                        else
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+                    }
                 }
                 else
                 {
-                    ShotGun(item);
-                }
-            }
-        }
-
-        private static void ShotGun(Item item)
-        {
-            if (!item.Name.Equals("Aged Brie")
-                && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-            {
-                if (item.Quality > 0)
-                {
-                    if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
-            {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (!item.Name.Equals("Aged Brie"))
-                {
-                    if (!item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                    if (!item.Name.Equals("Aged Brie")
+                        && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
                     {
                         if (item.Quality > 0)
                         {
@@ -136,14 +152,62 @@
                     }
                     else
                     {
-                        item.Quality = item.Quality - item.Quality;
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+
+                            if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                            {
+                                if (item.SellIn < 11)
+                                {
+                                    if (item.Quality < 50)
+                                    {
+                                        item.Quality = item.Quality + 1;
+                                    }
+                                }
+
+                                if (item.SellIn < 6)
+                                {
+                                    if (item.Quality < 50)
+                                    {
+                                        item.Quality = item.Quality + 1;
+                                    }
+                                }
+                            }
+                        }
                     }
-                }
-                else
-                {
-                    if (item.Quality < 50)
+
+                    if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
                     {
-                        item.Quality = item.Quality + 1;
+                        item.SellIn = item.SellIn - 1;
+                    }
+
+                    if (item.SellIn < 0)
+                    {
+                        if (!item.Name.Equals("Aged Brie"))
+                        {
+                            if (!item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                            {
+                                if (item.Quality > 0)
+                                {
+                                    if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                                    {
+                                        item.Quality = item.Quality - 1;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                item.Quality = item.Quality - item.Quality;
+                            }
+                        }
+                        else
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
                     }
                 }
             }
