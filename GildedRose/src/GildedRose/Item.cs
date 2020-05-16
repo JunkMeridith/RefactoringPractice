9d8183b6d51fa1e30c32737e76556dfd8d48c479
MeridithJunk <Meridith.a.junk@accenture.com>
2020-05-16 17:58:55 -0500

    Large class - moving aged Brie to it's own class

--- GildedRose/src/GildedRose/Item.cs
+++ GildedRose/src/GildedRose/Item.cs
@@ -47,27 +47,4 @@ namespace GildedRose
             }
         }
     }
-
-
-    class AgedBrie : Item
-    {
-        protected internal AgedBrie(int sellIn, int quality) : base("Aged Brie", sellIn, quality)
-        {
-        }
-
-        public override void UpdateItem()
-        {
-            if (Quality < 50)
-            {
-                Quality = Quality + 1;
-            }
-
-            SellIn = SellIn - 1;
-
-            if (SellIn < 0 && Quality < 50)
-            {
-                Quality = Quality + 1;
-            }
-        }
-    }
 }
