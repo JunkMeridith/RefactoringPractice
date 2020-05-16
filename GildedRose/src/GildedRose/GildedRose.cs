7fc7e1d9065ad26cea5d83810cd7569fea4465d9
MeridithJunk <Meridith.a.junk@accenture.com>
2020-05-16 16:34:55 -0500

    Long Method: Decomposing conditional, by breaking down the long method Sulfuras

--- GildedRose/src/GildedRose/GildedRose.cs
+++ GildedRose/src/GildedRose/GildedRose.cs
@@ -64,10 +64,67 @@
                         item.Quality = item.Quality - item.Quality;
                     }
                 }
+                else if (item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                {
+                    ShotGun(item);
+                }
                 else
                 {
-                    if (!item.Name.Equals("Aged Brie")
-                        && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                    ShotGun(item);
+                }
+            }
+        }
+
+        private static void ShotGun(Item item)
+        {
+            if (!item.Name.Equals("Aged Brie")
+                && !item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+            {
+                if (item.Quality > 0)
+                {
+                    if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (!item.Name.Equals("Aged Brie"))
+                {
+                    if (!item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
                     {
                         if (item.Quality > 0)
                         {
@@ -79,62 +136,14 @@
                     }
                     else
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-
-                            if (item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality = item.Quality + 1;
-                                    }
-                                }
-                            }
-                        }
+                        item.Quality = item.Quality - item.Quality;
                     }
-
-                    if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+                }
+                else
+                {
+                    if (item.Quality < 50)
                     {
-                        item.SellIn = item.SellIn - 1;
-                    }
-
-                    if (item.SellIn < 0)
-                    {
-                        if (!item.Name.Equals("Aged Brie"))
-                        {
-                            if (!item.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-                            {
-                                if (item.Quality > 0)
-                                {
-                                    if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
-                                    {
-                                        item.Quality = item.Quality - 1;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                item.Quality = item.Quality - item.Quality;
-                            }
-                        }
-                        else
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
